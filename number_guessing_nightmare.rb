dc200bb17409f25a06aff29dd9795b3160b4a860
Julie David <julie.angela.david@gmail.com>
2015-09-02 09:35:11 -0400

    Have feature to check if user is lying.

--- number_guessing_nightmare.rb
+++ number_guessing_nightmare.rb
@@ -2,15 +2,25 @@
 
 h = (1..100).to_a
 n = [33, 66].shuffle.sample
-#puts "#{h}"
+# puts "#{h}"
 
 def ask(question)
   puts question
   gets.chomp
 end
 
+def check(x, num)
+  if !x.include?(num)
+    puts 'User, you have lied to the computer. You have brought dishonor on your family.'
+    return false
+  else
+    return true
+  end
+end
+
 5.times do
   input = ask("Is your number #{n}?").upcase
+
   if input == 'YES'
     puts 'Computer wins'
     break
@@ -34,11 +44,14 @@ end
       else
         high = ask('User, please enter higher or lower.').upcase
       end
+      break if check(h, n) == false
     end
 
-    #puts "#{h}"
+    puts "#{h}"
 
     n = h[((h.count) / 3)]
+    break if check(h, n) == false
     # n should be the index of h at the length of h divided by 3
   end
+  break if check(h, n) == false
 end
