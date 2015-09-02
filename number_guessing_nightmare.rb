dfec14454221a0231ca412215411d74b98a72ea6
Julie David <julie.angela.david@gmail.com>
2015-09-02 01:40:38 -0400

    Have some features work sometimes

--- number_guessing_nightmare.rb
+++ number_guessing_nightmare.rb
@@ -3,33 +3,42 @@
 h = (1..100).to_a
 n = [33, 66].shuffle.sample
 puts "#{h}"
-5.times do
-  puts "Is your number #{n}?"
 
-  if gets.chomp.upcase == 'YES'
-    puts 'Computer wins'
-    break
-  else gets.chomp.upcase == 'NO'
-       puts 'Is your number higher or lower?'
-       if gets.chomp.upcase == 'HIGHER'
-         h = h.each do |x|
-           delete_if x <= n
-           #all numbers less than or equal to n are deleted from h
-         end
-       elsif gets.chomp.upcase == 'LOWER'
-         h = h.each do |x|
-           delete_if x >= n
-           #all numbers greater than or equal to n are deleted from h
-         end
-       else
-         'User, please enter higher or lower.'
-       end
-    end
-      #"User put the correct input pls"
-       puts "#{h}"
-
-  n = h[((h.count) / 3)]
-  #n should be the index of h at the length of h divided by 3
+def ask(question)
+  puts question
+  gets.chomp
 end
 
-# Atom plea
+5.times do
+  input = ask("Is your number #{n}?").upcase
+  if input == 'YES'
+    puts 'Computer wins'
+    break
+  else
+    loop do
+      high = ask('Is your number higher or lower?').upcase
+
+      if high == 'HIGHER'
+        h.delete_if do |x|
+          x <= n
+          # all numbers less than or equal to n are deleted from h
+        end
+        break
+      elsif high == 'LOWER'
+        h.delete_if do |x|
+          x >= n
+          # all numbers greater than or equal to n are deleted from h
+        end
+        break
+
+      else
+        high = ask('User, please enter higher or lower.').upcase
+      end
+    end
+
+    puts "#{h}"
+
+    n = h[((h.count) / 3)]
+    # n should be the index of h at the length of h divided by 3
+  end
+end
