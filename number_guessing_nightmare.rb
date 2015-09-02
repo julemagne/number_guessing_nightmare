111253cbc09b9b5b8954685074dd00ab0e5762aa
Julie David <julie.angela.david@gmail.com>
2015-09-02 00:56:19 -0400

    Makes range into array

--- number_guessing_nightmare.rb
+++ number_guessing_nightmare.rb
@@ -11,13 +11,13 @@ n = [33, 66].shuffle.sample
   else gets.chomp.upcase == 'NO'
        puts 'Is your number higher or lower?'
        if gets.chomp.upcase == 'HIGHER'
-         h = h.each do|x|
-           x.delete if x < n || x = n
+         h = h.each do |x|
+           x.delete if x < n || x == n
            #all numbers less than or equal to n are deleted from h
          end
        elsif gets.chomp.upcase == 'LOWER'
-         h = h.each do|x|
-           x.delete if x > n || x = n
+         h = h.each do |x|
+           x.delete if x > n || x == n
            #all numbers greater than or equal to n are deleted from h
          end
        else
@@ -26,7 +26,7 @@ n = [33, 66].shuffle.sample
     end
       #"User put the correct input pls"
        puts "#{h}"
-  
+
   n = h[((h.count) / 3)]
   #n should be the index of h at the length of h divided by 3
 end
